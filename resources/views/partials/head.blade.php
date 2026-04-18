d4895b3027f7024e0dfee0266a0c16677c777145
Dennis Rehbehn <dennisrehbehn@googlemail.com>
2026-04-18 22:37:20 +0200

    feat: add Content-Security-Policy upgrade-insecure-requests meta tag and improve title formatting

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,12 +2,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>
-    {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
+    {{ filled($title ?? null) ? $title . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
